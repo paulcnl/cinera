ac6391c98f14ac6a7f3d8df6cea9ac1927b337d8
paulcnl <paul.canarelli.2504@gmail.com>
2024-12-02 16:30:16 +0100

    category

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,45 @@
+// Some data to make the trick
+
+const categories = [
+    {
+      id: 1,
+      name: "Comédie",
+    },
+    {
+      id: 2,
+      name: "Science-Fiction",
+    },
+  ];
+  
+  // Declare the actions
+  
+  import type { RequestHandler } from "express";
+
+  const browse: RequestHandler = (req, res) => {
+    if (req.query.q != null) {
+      const filteredCategories = categories.filter((c) =>
+        c.name.includes(req.query.q as string),
+      );
+  
+      res.json(filteredCategories);
+    } else {
+      res.json(categories);
+    }
+  };
+  
+  const read: RequestHandler = (req, res) => {
+    const parsedId = Number.parseInt(req.params.id);
+  
+    const category = categories.find((c) => c.id === parsedId);
+  
+    if (category) {
+      res.json(category);
+    } else {
+      res.sendStatus(404);
+    }
+  };
+  
+  // Export it to import it somewhere else
+  
+  export default { browse, read };
+  
--- server/src/router.ts
+++ server/src/router.ts
@@ -13,13 +13,23 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+// Define program-related routes
+import programActions from "./modules/item/programActions";
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/programs/", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+router.get("/api/categories/", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
+
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
 
-import browse from "./modules/item/programActions";
+import sayActions from "./modules/item/sayActions";
 
-router.get("/api/programs", browse.browse);
+router.get("/", sayActions.sayWelcome);
 
 /* ************************************************************************* */
 
